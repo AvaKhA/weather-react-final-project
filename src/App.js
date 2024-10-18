ae6f85303c61905f9f51da50a9c95f8e2fbe6101
AvaKhA <109901314+AvaKhA@users.noreply.github.com>
2024-10-18 19:17:49 +0330

    added bootstrap and weather component and CSS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This Project was coded by Ava Khalili and is{" "}
-        <a
-          href="https://github.com/AvaKhA/weather-react-final-project"
-          target="_blank"
-        >
-          open-sourced on GitHub!
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This Project was coded by Ava Khalili and is{" "}
+          <a
+            href="https://github.com/AvaKhA/weather-react-final-project"
+            target="_blank"
+          >
+            open-sourced on GitHub!
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
