503573f75a0ff782d01b114e321c45a9859060a4
AvaKhA <109901314+AvaKhA@users.noreply.github.com>
2024-10-18 18:25:42 +0330

    added footer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Weather
+      <h1>Weather App</h1>
+      <footer>
+        This Project was coded by Ava Khalili and is{" "}
+        <a
+          href="https://github.com/AvaKhA/weather-react-final-project"
+          target="_blank"
+        >
+          open-sourced on GitHub!
+        </a>
+      </footer>
     </div>
   );
 }
