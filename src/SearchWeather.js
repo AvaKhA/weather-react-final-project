e90b5477c26068a0229197a915d183dcade085ac
AvaKhA <109901314+AvaKhA@users.noreply.github.com>
2024-10-28 20:43:52 +0330

    removed unused import

--- src/SearchWeather.js
+++ src/SearchWeather.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import "./Weather.css";
-import FormatDate from "./FormatDate";
 import { useState } from "react";
 import WeatherContent from "./WeatherContent";
 import WeatherForecast from "./WeatherForecast";
